0b3c651ed2d32d5c1f260d5a765fd1be7b893436
Elizabethsss <51675488+Elizabethsss@users.noreply.github.com>
2019-09-23 17:53:26 +0300

    Pig Latin

--- Text/01/Process.java
+++ Text/01/Process.java
@@ -5,21 +5,26 @@ class Process {
     private String input;
     private String vowels = "aeiou";
     private String result;
+    private String[] words;
     private char[] charBox;
 
     void process() {
         System.out.println("Pig Latin");
-        System.out.print("What word would you like to translate to Pig Latin?: ");
-        while ((input = in.next()) != null) {
-            result = input;
-            charBox = input.toCharArray();
-            for (char c : charBox) {
-                if (vowels.indexOf(c) == -1) {
-                    result += c;
-                    result = result.substring(1);
-                } else break;
+        System.out.print("Enter a string: ");
+        if ((input = in.nextLine()) != null) {
+            words = input.split(" ");
+            for (String str : words) {
+                result = str;
+                charBox = str.toCharArray();
+                for (char c : charBox) {
+                    if (vowels.indexOf(c) == -1) {
+                        result += c;
+                        result = result.substring(1);
+                    } else break;
+                }
+                System.out.print(result + "ay ");
             }
-            System.out.print(result + "ay ");
         }
+        System.out.println();
     }
 }
